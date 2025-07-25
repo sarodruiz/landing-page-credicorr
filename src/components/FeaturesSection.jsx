440134a9b3dab2ffded2b5b837712c2c6b9d2bbc
sarodruiz <sarodruiz@gmail.com>
2025-07-25 11:41:07 -0300

    improved features section

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -4,19 +4,20 @@ function FeaturesSection() {
       <div className="max-w-5xl mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10 gap-y-12 text-center md:text-left">
           {/* Feature 1 */}
-          <div className="flex flex-col items-center text-center md:items-start md:text-left">
+          <div className="flex flex-col items-center text-center md:items-center md:text-center">
             <div className="text-green-600 mb-4">
               <svg
-                className="w-12 h-12"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
+                stroke-width="1.5"
                 stroke="currentColor"
+                class="size-12"
               >
-                <path strokeWidth="1.5" d="M12 12a4 4 0 100-8 4 4 0 000 8z" />
                 <path
-                  strokeWidth="1.5"
-                  d="M12 14c-4 0-6 2-6 4v2h12v-2c0-2-2-4-6-4z"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
                 />
               </svg>
             </div>
@@ -29,17 +30,21 @@ function FeaturesSection() {
           </div>
 
           {/* Feature 2 */}
-          <div className="flex flex-col items-center text-center md:items-start md:text-left">
+          <div className="flex flex-col items-center text-center md:items-center md:text-center">
             <div className="text-green-600 mb-4">
               <svg
-                className="w-12 h-12"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
+                stroke-width="1.5"
                 stroke="currentColor"
+                class="size-12"
               >
-                <path strokeWidth="1.5" d="M7 4h10v16H7z" />
-                <path strokeWidth="1.5" d="M9 2v2m6-2v2M7 20h10" />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
+                />
               </svg>
             </div>
             <h3 className="font-semibold text-lg mb-2 text-blue-900">
@@ -52,20 +57,20 @@ function FeaturesSection() {
           </div>
 
           {/* Feature 3 */}
-          <div className="flex flex-col items-center text-center md:items-start md:text-left">
+          <div className="flex flex-col items-center text-center md:items-center md:text-center">
             <div className="text-green-600 mb-4">
               <svg
-                className="w-12 h-12"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
+                stroke-width="1.5"
                 stroke="currentColor"
+                class="size-12"
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 13l4 4L19 7"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
                 />
               </svg>
             </div>
